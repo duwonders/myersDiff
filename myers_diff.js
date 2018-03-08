d8ebcccafab1dd16217f15f3047986bab3ddfa3d
duwonders <duwonders@gmail.com>
2018-03-08 20:18:08 +0800

    dsa

--- myers_diff.js
+++ myers_diff.js
@@ -1,3 +1,25 @@
-function diff() {
-  
+function diff(set1, set2) {
+  const max_y = set1.length - 1;
+  const max_x = set2.length - 1;
+  const max_steps = set1.length + set2.length;
+  let stepPoints = [[0, 0]];
+  function goSteps() {
+    let nextStpePoints = [];
+    stepPoints.map(point => {
+      let res = step(point);
+      if(typeof(res) === "object"){
+        res.forEach(i => {
+          nextStpePoints.push(i);
+        });
+      } 
+      else {
+        
+      }
+    })
+    stepPoints = nextStpePoints;
+    return goSteps();
+  }
+  function step(point) {
+    
+  }
 }
